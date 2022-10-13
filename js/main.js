9e7f73afa3605cce2078ec47d455b4d6b1f3b04c
Johannes Busching <b1tsteller@users.noreply.github.com>
2022-10-13 12:34:43 +0200

    cleaned up and sanitized, bugfixes

--- js/main.js
+++ js/main.js
@@ -3,97 +3,51 @@ import { Taggy } from "../../taggy-base/taggy/lib/index";
 // create instance of taggy
 let taggyObject = new Taggy();
 
-console.log(taggyObject.hello());
+// set input field for taggy
+let inputFieldForTaggy = document.getElementById("taggyInput");
+taggyObject.setInputField(inputFieldForTaggy);
+console.log(inputFieldForTaggy);
 
-// setup HTML
-// let divWrapper = document.createElement("div");
-// divWrapper.id = "results";
-// let outputField = document.createElement("input");
-// outputField.id = "output";
-// divWrapper.appendChild(outputField);
-// document.body.appendChild(divWrapper);
-
-// frequency output
-// let frequencySpan = document.createElement("span");
-// divWrapper.appendChild(frequencySpan);
-
-let outputFieldForTaggy = document.getElementById("output");
-console.log(outputFieldForTaggy);
-
+// set output field for taggy
+let outputFieldForTaggy = document.getElementById("taggyOutput");
 taggyObject.setOutputField(outputFieldForTaggy);
-
-// var tagify = new Tagify(outputFieldForTaggy);
-taggyCreate("myTaggy", document.body, outputFieldForTaggy);
+console.log(outputFieldForTaggy);
 
 // loading animation
 let divWrapperLoading = document.createElement("div");
 divWrapperLoading.className = "loader";
 divWrapperLoading.style.display = "none";
-
 document.getElementById("myTaggy").appendChild(divWrapperLoading);
 
-function taggyCreate(id, parent, outputFieldForTaggy) {
-  let divWrapper = document.createElement("div");
-  let inputField = document.createElement("textarea");
-  taggyObject.setInputField(inputField);
-  divWrapper.setAttribute("type", "text");
-  divWrapper.appendChild(inputField);
-  divWrapper.id = id;
+let timeout = null;
 
-  //   let canvasElem = document.createElement("canvas");
-  parent.appendChild(divWrapper);
-  //   divWrapper.appendChild(canvasElem);
+// setup processing after feedback input -> TOTO: put into taggy
+inputFieldForTaggy.addEventListener("input", async function (event) {
+  clearTimeout(timeout);
 
-  //   canvasElem.width = width;
-  //   canvasElem.height = height;
+  // make a new timeout set to go off in 1000ms
+  timeout = setTimeout(async function () {
+    divWrapperLoading.style.display = "block";
+    // createSimpleTag(taggy.taggyVanilla(inputField.value));
+    // let result = await taggyObject.processInput(inputFieldForTaggy.value);
+    divWrapperLoading.style.display = "none";
 
-  //   let ctx = canvasElem.getContext("2d");
+    let result = await taggyObject.processAndAddTags(
+      inputFieldForTaggy.value,
+      outputFieldForTaggy
+    );
 
-  let timeout = null;
+    if (result && !result.includes(undefined)) {
+      let frequencySpan = document.getElementById("frequency");
 
-  inputField.addEventListener("input", async function (event) {
-    clearTimeout(timeout);
+      frequencySpan.innerHTML =
+        "top candidates: " + (await taggyObject.getMostFrequent().join(" "));
+    } else {
+      frequencySpan.innerHTML = "No match with glossar";
+    }
+  }, 1000);
 
-    // Make a new timeout set to go off in 1000ms (1 second)
-    timeout = setTimeout(async function () {
-      divWrapperLoading.style.display = "block";
-      // console.log("INPUTFIELDVALUE");
-      // console.log(inputField.value);
-
-      // createSimpleTag(taggy.taggyVanilla(inputField.value));
-      let result = await taggyObject.processInput(inputField.value);
-      divWrapperLoading.style.display = "none";
-
-      taggyObject.processAndAddTags(inputField.value, outputFieldForTaggy);
-
-      // let inputElementForTagify = await taggyObject.createTagify(
-      //   outputFieldForTaggy
-      // );
-
-      // await taggyObject.deleteTags();
-
-      console.log("result", result);
-      console.log("result.length", result.length);
-      if (!result.includes(undefined)) {
-        // let addedTag = await taggyObject.addTags(result);
-
-        // outputFieldForTaggy.value = result;
-
-        let frequencySpan = document.getElementById("frequency");
-        console.log("FREQ", frequencySpan);
-
-        frequencySpan.innerHTML =
-          "top candidates: " + (await taggyObject.getMostFrequent().join(" "));
-      } else {
-        frequencySpan.innerHTML = "No match with glossar";
-      }
-      // console.log(addedTag);
-
-      // createSimpleTag(inputField.value);
-    }, 1000);
-
-    return {
-      inputField: inputField,
-    };
-  });
-}
+  return {
+    inputField: inputFieldForTaggy,
+  };
+});
