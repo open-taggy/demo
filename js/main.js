d6372816b241e01c885ef8d8548c0e614b076bcc
Johannes Busching <b1tsteller@users.noreply.github.com>
2023-01-17 17:20:10 +0100

    added example buttons, data and logic, added toggle to glossary and options-container, enhanced styles and wording

--- js/main.js
+++ js/main.js
@@ -41,7 +41,13 @@ jQuery(function () {
     loaderDiv
   );
 
+  // use submit button instead of auto-detection
+  taggyObject.config.use_submit = true;
+
   // console.log(taggyObject.config);
+
+  let optionsToggle = document.getElementById("optionsToggle");
+  let glossaryToggle = $(".glossary-toggle");
 
   let taggyConfig = Object.keys(taggyObject.config);
   console.log("CONFIG", taggyConfig);
@@ -62,34 +68,63 @@ jQuery(function () {
 
     switch (selectedTabId) {
       case "tab-1":
-        switchGlossary("1", glossaryAbo);
+        example1 =
+          "Ich habe eure Zeitung bestellt und sie landet nie da, wo sie hin soll. Das ist eine Frechheit!";
+        example2 =
+          "Guten Tag. Wir verreisen ins Ausland. Deswegen bitte um Nachsendung der Zeitung an die neue Adresse: ...";
+        example3 =
+          "Hallo. Auf meinem ereader wird das PDF einfach nicht richtig dargestellt. Bitte um Rückmeldung.";
+        switchGlossary("1", glossaryAbo, example1, example2, example3);
         break;
       case "tab-2":
-        switchGlossary("2", glossaryNewspaper);
+        example1 =
+          "Pepperoni pizza is a classic favorite among pizza lovers. It is made with a tomato sauce base, cheese, and slices of pepperoni, a type of spicy salami. Pepperoni is a popular topping due to its spicy and savory flavor. Whether you're ordering in or making your own, it's a delicious and satisfying meal.";
+        example2 =
+          "Environmental awareness is the understanding and recognition of the impact of human actions on the natural world. It is important to be mindful of the ways in which our daily choices and habits can harm the environment and make efforts to reduce our impact. Simple actions such as reducing, reusing, and recycling can make a significant difference in preserving the planet for future generations. By raising environmental awareness, we can work together to create a sustainable future for all.";
+        example3 =
+          "A growth recession is a period of economic decline characterized by low or negative GDP growth. It is different from a typical recession in which there is a significant decrease in economic activity, but still positive GDP growth. Economic growth recessions are often caused by a combination of factors such as global economic slowdown, tight monetary policies, and political instability. To mitigate the effects of growth recession, governments and central banks may implement policies such as monetary stimulus and fiscal stimulus.";
+        switchGlossary("2", glossaryNewspaper, example1, example2, example3);
         break;
       case "tab-3":
-        switchGlossary("3", glossaryShop);
+        switchGlossary("3", glossaryShop, example1, example2, example3);
+        presentInput = "";
         break;
     }
 
-    function switchGlossary(number, glossaryData) {
+    function switchGlossary(
+      number,
+      glossaryData,
+      example1,
+      example2,
+      example3
+    ) {
+      $("#taggyInput").val(example1);
+      $("#example1").data("text", example1);
+      $("#example2").data("text", example2);
+      $("#example3").data("text", example3);
+
+      taggyObject.deleteTags();
       taggyObject.setGlossary(glossaryData);
 
       if (!$("#tab-content-" + number + " #container-glossary").length) {
         console.log("#container-glossary IS NOT inside #tab-content-" + number);
 
-        $("#tab-content-" + number).append(
-          $("<div></div>").attr("id", "container-glossary")
-        );
-
         let glossaryDataPrint = JSON.stringify(glossaryData, null, 2); // spacing level = 2
-        $("#tab-content-" + number).append(
-          $("<pre></pre>")
-            .addClass(
-              "mt-2 rounded-md outline outline-offset-1 outline-2 outline-gray-400 text-[0.66rem]"
-            )
-            .text(glossaryDataPrint)
-        );
+        // $("#tab-content-" + number)
+        //   .find(".glossary-content")
+        //   .append($("<div></div>").attr("id", "container-glossary"));
+
+        let preElement = $("<pre id='glossary-pre'></pre>")
+          .addClass(
+            "mt-2 rounded-md outline outline-offset-1 outline-2 outline-gray-400 text-[0.66rem] hidden"
+          )
+          .text(glossaryDataPrint);
+
+        $("#glossary-pre").remove();
+
+        $("#tab-content-" + number)
+          .find(".glossary-content")
+          .append(preElement);
       }
     }
   });
@@ -153,6 +188,28 @@ jQuery(function () {
 
   $(submitButton).on("click", function () {
     $("#extras").addClass("hidden");
+  });
+
+  $(".button-example").on("click", function () {
+    taggyObject.deleteTags();
+    console.log("this", this);
+    console.log("data-text", $(this).data("text"));
+    $("#taggyInput").val($(this).data("text"));
+  });
+
+  // options toggle functionality
+  $(optionsToggle).on("click", function () {
+    $("#container-options").toggleClass("hidden");
+    $("#options-chevron-left").toggleClass("hidden");
+    $("#options-chevron-down").toggleClass("hidden");
+  });
+
+  // glossary toggle functionality
+  $(glossaryToggle).on("click", function () {
+    console.log("glossaryToggle clicked");
+    $("#glossary-pre").toggleClass("hidden");
+    $("#glossary-chevron-left").toggleClass("hidden");
+    $("#glossary-chevron-down").toggleClass("hidden");
   });
 
   // give every new tag a random color
