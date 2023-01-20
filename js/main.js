862d57e21541d44c5aefde3fa415bd5834928355
Johannes Busching <b1tsteller@users.noreply.github.com>
2023-01-20 11:55:22 +0100

    enhanced wording, structure and styles; bugfixes

--- js/main.js
+++ js/main.js
@@ -69,21 +69,21 @@ jQuery(function () {
     switch (selectedTabId) {
       case "tab-1":
         example1 =
+          "Pepperoni pizza is a classic favorite among pizza lovers. It is made with a tomato sauce base, cheese, and slices of pepperoni, a type of spicy salami. Pepperoni is a popular topping due to its spicy and savory flavor. Whether you're ordering in or making your own, it's a delicious and satisfying meal.";
+        example2 =
+          "Environmental awareness is the understanding and recognition of the impact of human actions on the natural world. It is important to be mindful of the ways in which our daily choices and habits can harm the environment and make efforts to reduce our impact. Simple actions such as reducing, reusing, and recycling can make a significant difference in preserving the planet for future generations. By raising environmental awareness, which is also part of the politics, we can work together to create a sustainable future for all.";
+        example3 =
+          "A growth recession is a period of economic decline characterized by low or negative GDP growth. It is different from a typical recession in which there is a significant decrease in economic activity, but still positive GDP growth. Economic growth recessions are often caused by a combination of factors such as global economic slowdown, tight monetary policies, and political instability. To mitigate the effects of growth recession, governments and central banks may implement policies such as monetary stimulus and fiscal stimulus.";
+        switchGlossary("1", glossaryNewspaper, example1, example2, example3);
+        break;
+      case "tab-2":
+        example1 =
           "Ich habe eure Zeitung bestellt und sie landet nie da, wo sie hin soll. Das ist eine Frechheit!";
         example2 =
           "Guten Tag. Wir verreisen ins Ausland. Deswegen bitte um Nachsendung der Zeitung an die neue Adresse: ...";
         example3 =
           "Hallo. Auf meinem ereader wird das PDF einfach nicht richtig dargestellt. Bitte um Rückmeldung.";
-        switchGlossary("1", glossaryAbo, example1, example2, example3);
-        break;
-      case "tab-2":
-        example1 =
-          "Pepperoni pizza is a classic favorite among pizza lovers. It is made with a tomato sauce base, cheese, and slices of pepperoni, a type of spicy salami. Pepperoni is a popular topping due to its spicy and savory flavor. Whether you're ordering in or making your own, it's a delicious and satisfying meal.";
-        example2 =
-          "Environmental awareness is the understanding and recognition of the impact of human actions on the natural world. It is important to be mindful of the ways in which our daily choices and habits can harm the environment and make efforts to reduce our impact. Simple actions such as reducing, reusing, and recycling can make a significant difference in preserving the planet for future generations. By raising environmental awareness, we can work together to create a sustainable future for all.";
-        example3 =
-          "A growth recession is a period of economic decline characterized by low or negative GDP growth. It is different from a typical recession in which there is a significant decrease in economic activity, but still positive GDP growth. Economic growth recessions are often caused by a combination of factors such as global economic slowdown, tight monetary policies, and political instability. To mitigate the effects of growth recession, governments and central banks may implement policies such as monetary stimulus and fiscal stimulus.";
-        switchGlossary("2", glossaryNewspaper, example1, example2, example3);
+        switchGlossary("2", glossaryAbo, example1, example2, example3);
         break;
       case "tab-3":
         switchGlossary("3", glossaryShop, example1, example2, example3);
@@ -100,10 +100,13 @@ jQuery(function () {
     ) {
       $("#taggyInput").val(example1);
       $("#example1").data("text", example1);
+      $("#example1").prop("disabled", true);
+      $("#example1").addClass("opacity-50 cursor-not-allowed");
       $("#example2").data("text", example2);
       $("#example3").data("text", example3);
 
       taggyObject.deleteTags();
+      $("#extras").addClass("hidden");
       taggyObject.setGlossary(glossaryData);
 
       if (!$("#tab-content-" + number + " #container-glossary").length) {
@@ -192,8 +195,13 @@ jQuery(function () {
 
   $(".button-example").on("click", function () {
     taggyObject.deleteTags();
+    $("#extras").addClass("hidden");
     console.log("this", this);
     console.log("data-text", $(this).data("text"));
+    $(".button-example").removeClass("opacity-50 cursor-not-allowed");
+    $(".button-example").prop("disabled", false);
+    $(this).prop("disabled", true);
+    $(this).addClass("opacity-50 cursor-not-allowed");
     $("#taggyInput").val($(this).data("text"));
   });
 
@@ -232,7 +240,7 @@ jQuery(function () {
       const overrideTitle = $("<h3></h3>")
         .attr("id", "override-title")
         .text("Multiple possibilities found. Click to override main tag:")
-        .addClass("pr-8");
+        .addClass("pr-8 text-sm");
       $("#override").prepend(overrideTitle);
     }
   });
